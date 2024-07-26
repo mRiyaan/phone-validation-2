3d09d966cfd77e32be4142295a7be5d774f5ab10
mRiyaan <mohammedriyaan115@gmail.com>
2024-07-26 18:31:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    validatePhoneNumber();
+});
+
+function validatePhoneNumber() {
+    const phonenumber= getElementById('phone');
+    
+   
+}
